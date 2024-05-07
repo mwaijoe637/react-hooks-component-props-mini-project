35d656be3ea52eba8eeaabd1e5fdc2483f350909
joe <brakejoe9@gmail.com>
2024-05-07 16:44:41 +0300

    done

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
-// Article.js
+
 import React from "react";
 
 function Article({ title, date, preview, minutes }) {
-  // Calculate the number of emojis based on the minutes
+  
   const emojis = minutes < 30 ? '☕️'.repeat(Math.ceil(minutes / 5)) : '🍱'.repeat(Math.ceil(minutes / 10));
 
-  // Format the date or use default if not provided
+  
   const formattedDate = date ? date : "January 1, 1970";
 
   return (
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,4 @@
-// ArticleList.js
+
 import React from "react";
 import Article from "./Article";
 
